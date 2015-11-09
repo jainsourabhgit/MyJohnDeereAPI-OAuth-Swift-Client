f6e447e2b51fd05bf81f5bc3f0781733c71e308b
Jain Sourabh <sj96604@wdxrw575033qj.jdnet.deere.com>
2015-11-09 13:48:12 -0600

    Initial version

---
+++ OAuthSwiftDemo/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  OAuthSwift
+//
+//  Created by Dongri Jin on 7/17/14.
+//  Copyright (c) 2014 Dongri Jin. All rights reserved.
+//
+
+import Foundation
+
+let MyJohnDeere =
+[
+    "consumerKey": "Please enter client key here...",
+    "consumerSecret": "Please enter client secret here..."
+]
